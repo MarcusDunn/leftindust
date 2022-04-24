0e7449ac6cc3b379560ee5a994755f0913819545
Marcus Dunn <51931484+MarcusDunn@users.noreply.github.com>
2022-04-24 13:11:30 -0700

    added svelte check to ultra build (#46)

--- ultra/build.gradle.kts
+++ ultra/build.gradle.kts
@@ -1,3 +1,12 @@
+import com.github.gradle.node.yarn.task.YarnTask
+
 plugins {
-	id("com.github.node-gradle.node") version "3.2.1"
+    id("com.github.node-gradle.node") version "3.2.1"
+}
+
+val svelteCheck by tasks.register("svelteCheck", YarnTask::class) {
+    description =
+        "Provides CLI diagnostics checks for Unused CSS, Svelte A11y hints, and JavaScript/TypeScript compiler errors"
+    dependsOn(tasks.yarn)
+    args.set(listOf("run", "check:ultra"))
 }
