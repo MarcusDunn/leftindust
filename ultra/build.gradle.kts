ee1b0424073223dfd49403db0a6c98cbeaf12969
Marcus Dunn <51931484+MarcusDunn@users.noreply.github.com>
2022-04-25 19:21:20 -0700

    Update build.gradle.kts

--- ultra/build.gradle.kts
+++ ultra/build.gradle.kts
@@ -10,3 +10,9 @@ val svelteCheck by tasks.register("svelteCheck", YarnTask::class) {
     dependsOn(tasks.yarn)
     args.set(listOf("run", "check:ultra"))
 }
+
+val generate by tasks.register("generate", YarnTask::class) {
+    description = "checks written queries and fragments are valid"
+    dependsOn(tasks.yarn)
+    args.set(listOf("run", "ultra:generate"))
+}
