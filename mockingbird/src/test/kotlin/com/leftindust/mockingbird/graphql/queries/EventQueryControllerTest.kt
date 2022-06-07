04d410c04778e290a30fd07ec3a13b6282a14ade
ashgozli <ak23b24@gmail.com>
2022-06-06 22:35:36 -0700

    Tested getByIds method of EventQueryController

---
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/graphql/queries/EventQueryControllerTest.kt
@@ -0,0 +1,87 @@
+package com.leftindust.mockingbird.graphql.queries
+
+import com.leftindust.mockingbird.event.*
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class EventQueryControllerUnitTest () {
+    private val readEventService = mockk<ReadEventServiceImpl>()
+    private val eventToEventDtoConverter = EventToEventDtoConverter()
+    private val eventQueryController = EventQueryController(readEventService,eventToEventDtoConverter)
+
+    @Test
+    @kotlinx.coroutines.ExperimentalCoroutinesApi
+    internal fun `check if eventIds on eventQueryController returns a list of queried users`() = runTest {
+        val eventIds = listOf(
+            EventDto.EventDtoId(UUID.randomUUID()),
+            EventDto.EventDtoId(UUID.randomUUID()),
+            EventDto.EventDtoId(UUID.randomUUID())
+        )
+
+        val event1 = mockk<Event>(relaxed = true )
+        val event2 = mockk<Event>(relaxed = true )
+        val event3 = mockk<Event>(relaxed = true )
+        coEvery { readEventService.getByEventId(eventIds.first()) } returns event1
+        coEvery { readEventService.getByEventId(eventIds[1]) } returns event2
+        coEvery { readEventService.getByEventId(eventIds[2]) } returns event3
+
+        val result = eventQueryController.eventsByIds(eventIds.asFlow()).toList()
+        assertThat(result, Matchers.hasSize(3))
+        assertThat(result.first(), Matchers.notNullValue())
+        assertThat(result[1], Matchers.notNullValue())
+        assertThat(result[2], Matchers.notNullValue())
+    }
+
+    @Test
+    @kotlinx.coroutines.ExperimentalCoroutinesApi
+    internal fun `check if eventIds returns a null`() = runTest {
+        val eventIds = listOf(
+            EventDto.EventDtoId(UUID.randomUUID()),
+            EventDto.EventDtoId(UUID.randomUUID()),
+            EventDto.EventDtoId(UUID.randomUUID())
+        )
+
+        val event1 = mockk<Event>(relaxed = true)
+        val event2 = null
+        val event3 = mockk<Event>(relaxed = true)
+        coEvery { readEventService.getByEventId(eventIds.first()) } returns event1
+        coEvery { readEventService.getByEventId(eventIds[1]) } returns event2
+        coEvery { readEventService.getByEventId(eventIds[2]) } returns event3
+
+        val result = eventQueryController.eventsByIds(eventIds.asFlow()).toList()
+        assertThat( result, Matchers.hasSize(3))
+        assertThat(result.first(), Matchers.notNullValue())
+        assertThat(result[1], Matchers.equalTo(null))
+        assertThat(result[2], Matchers.notNullValue())
+    }
+
+    @Test
+    @kotlinx.coroutines.ExperimentalCoroutinesApi
+    internal fun `check if queried eventsIds return null`() = runTest {
+        val eventIds = listOf(
+            EventDto.EventDtoId(UUID.randomUUID()),
+            EventDto.EventDtoId(UUID.randomUUID()),
+            EventDto.EventDtoId(UUID.randomUUID())
+        )
+
+        val event1 = null
+        val event2 = null
+        val event3 = null
+        coEvery { readEventService.getByEventId(eventIds.first()) } returns event1
+        coEvery { readEventService.getByEventId(eventIds[1]) } returns event2
+        coEvery { readEventService.getByEventId(eventIds[2]) } returns event3
+
+        val result = eventQueryController.eventsByIds(eventIds.asFlow()).toList()
+        assertThat( result, Matchers.hasSize(3))
+        assertThat( result.first(), Matchers.equalTo(null))
+        assertThat(result[1], Matchers.equalTo(null))
+        assertThat(result[2], Matchers.equalTo(null))
+    }
+}
