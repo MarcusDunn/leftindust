d6674a3b825e86fa6fa460aeb4a163dd091493c8
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-06-12 19:24:14 -0700

    small changes to make github axctions run again

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/graphql/queries/EventQueryControllerTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/graphql/queries/EventQueryControllerTest.kt
@@ -11,6 +11,7 @@ import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
 import java.util.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.extension.ExtendWith
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -22,7 +23,7 @@ internal class EventQueryControllerUnitTest {
 
     @Test
     internal fun `check if eventIds on eventQueryController returns a list of queried users`() = runTest {
-        val eventQueryController = EventQueryController(readEventService,eventToEventDtoConverter)
+        val eventQueryController = EventQueryController(readEventService, eventToEventDtoConverter)
 
         val eventIds = listOf(
             EventDto.EventDtoId(UUID.randomUUID()),
@@ -30,23 +31,21 @@ internal class EventQueryControllerUnitTest {
             EventDto.EventDtoId(UUID.randomUUID())
         )
 
-        val event1 = mockk<Event>(relaxed = true )
-        val event2 = mockk<Event>(relaxed = true )
-        val event3 = mockk<Event>(relaxed = true )
-        coEvery { readEventService.getByEventId(eventIds.first()) } returns event1
+        val event1 = mockk<Event>(relaxed = true)
+        val event2 = mockk<Event>(relaxed = true)
+        val event3 = mockk<Event>(relaxed = true)
+        coEvery { readEventService.getByEventId(eventIds[0]) } returns event1
         coEvery { readEventService.getByEventId(eventIds[1]) } returns event2
         coEvery { readEventService.getByEventId(eventIds[2]) } returns event3
 
         val result = eventQueryController.eventsByIds(eventIds).toList()
-        assertThat(result, Matchers.hasSize(3))
-        assertThat(result.first(), Matchers.notNullValue())
-        assertThat(result[1], Matchers.notNullValue())
-        assertThat(result[2], Matchers.notNullValue())
+        assertThat(result, hasSize(3))
+        assertThat(result, containsInRelativeOrder(notNullValue(), notNullValue(), notNullValue()))
     }
 
     @Test
     internal fun `check if eventIds returns a null`() = runTest {
-        val eventQueryController = EventQueryController(readEventService,eventToEventDtoConverter)
+        val eventQueryController = EventQueryController(readEventService, eventToEventDtoConverter)
 
         val eventIds = listOf(
             EventDto.EventDtoId(UUID.randomUUID()),
@@ -62,15 +61,15 @@ internal class EventQueryControllerUnitTest {
         coEvery { readEventService.getByEventId(eventIds[2]) } returns event3
 
         val result = eventQueryController.eventsByIds(eventIds).toList()
-        assertThat( result, Matchers.hasSize(3))
-        assertThat(result.first(), Matchers.notNullValue())
-        assertThat(result[1], Matchers.equalTo(null))
-        assertThat(result[2], Matchers.notNullValue())
+        assertThat(result, hasSize(3))
+        assertThat(result.first(), notNullValue())
+        assertThat(result[1], equalTo(null))
+        assertThat(result[2], notNullValue())
     }
 
     @Test
     internal fun `check if queried eventsIds return null`() = runTest {
-        val eventQueryController = EventQueryController(readEventService,eventToEventDtoConverter)
+        val eventQueryController = EventQueryController(readEventService, eventToEventDtoConverter)
 
         val eventIds = listOf(
             EventDto.EventDtoId(UUID.randomUUID()),
@@ -86,9 +85,9 @@ internal class EventQueryControllerUnitTest {
         coEvery { readEventService.getByEventId(eventIds[2]) } returns event3
 
         val result = eventQueryController.eventsByIds(eventIds).toList()
-        assertThat( result, Matchers.hasSize(3))
-        assertThat( result.first(), Matchers.equalTo(null))
-        assertThat(result[1], Matchers.equalTo(null))
-        assertThat(result[2], Matchers.equalTo(null))
+        assertThat(result, hasSize(3))
+        assertThat(result.first(), equalTo(null))
+        assertThat(result[1], equalTo(null))
+        assertThat(result[2], equalTo(null))
     }
 }
