12669f34a94dad79d51400f7cebf63d2c9197a81
Brian <86168500+briantkho@users.noreply.github.com>
2022-06-15 23:06:21 -0700

    Added nothing found status message when bundle is empty (#97)
    * Added nothing found to bundles
    index.ts, en.ts dictionary updated to standardized text
    DoctorsBundle, PatientsBundle added nothing found text
    DescriptivePlaceholder.scss added styling
    
    * formatting adjustment
    
    * Conditions showing no results found more specific
    Changed else to else if === 0 to avoid bugs
    
    * Removed Cells from DescriptivePlaceholder
    
    * Calls to the language file are changed to stores
    dot notation to "$_"
    
    Co-authored-by: Aydan Gaite <63032103+AydanGaite@users.noreply.github.com>

--- ultra/packages/leftindust/src/features/Patients/components/PatientsBundle/PatientsBundle.svelte
+++ ultra/packages/leftindust/src/features/Patients/components/PatientsBundle/PatientsBundle.svelte
@@ -12,6 +12,8 @@
   import Request from '@/features/Server/components/Request/Request.svelte';
   import Cells from '@/features/UI/components/Cells/Cells.svelte';
   import { Row, Col, Button } from 'framework7-svelte';
+  import language from "@/language/locales/en"
+import DescriptivePlaceholder from '@/features/App/components/DescriptivePlaceholder/DescriptivePlaceholder.svelte';
 
   let patients: PatientsFragment[] = [];
 
@@ -35,21 +37,29 @@
 >
   <Request {...$request} refetch={request.reexecute} middle>
     {#if patients.length > 0}
-    <Cells>
-      {#each patients as patient}
-        <PatientPinnableCell 
-          {patient}
-          pinned={pinned({
-            id: patient.pid.id,
-            type: patient.__typename,
-          }, data)}
-          on:pin={({ detail }) => pin(detail, {
-            id: patient.pid.id,
-            type: patient.__typename,
-          }, data)}
-        />
+      <Cells>
+        {#each patients as patient}
+          <PatientPinnableCell 
+            {patient}
+            pinned={pinned({
+              id: patient.pid.id,
+              type: patient.__typename,
+            }, data)}
+            on:pin={({ detail }) => pin(detail, {
+              id: patient.pid.id,
+              type: patient.__typename,
+            }, data)}
+          />
         {/each}
-    </Cells>
+      </Cells>
+    {:else}
+      <DescriptivePlaceholder 
+       title={$_('generics.noPatients')} 
+       description={$_('descriptions.noPatients')}
+       link = {{
+        label: $_('descriptions.learnMorePatients')
+       }}
+      />
     {/if}
   </Request>
 <Row slot="controls">
