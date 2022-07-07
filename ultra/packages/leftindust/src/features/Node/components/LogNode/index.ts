4c43173f4965642cbe4bed47066cdc370c8a9bf1
Victor <victoroltean09@gmail.com>
2022-07-07 14:43:31 -0700

    Added and registered log node.

---
+++ ultra/packages/leftindust/src/features/Node/components/LogNode/index.ts
@@ -0,0 +1,21 @@
+import type { NodeBlueprint, SocketBlueprint } from 'function-junctions/types';
+import LogNode from './LogNode.svelte';
+
+import numberSocket, { type NumberSocket } from '@/features/Socket/components/NumberSocket';
+
+const defaultBaseSocket: NumberSocket = {
+  ...numberSocket,
+  defaultValue: 4,
+}
+
+const node: NodeBlueprint<
+  { BASE: SocketBlueprint<number>, VALUE: SocketBlueprint<number> },
+  { Number: SocketBlueprint<number> }
+> = {
+  inputs: { BASE: defaultBaseSocket, VALUE: numberSocket },
+  outputs: { Number: numberSocket },
+  component: LogNode,
+  color: 'linear-gradient(#ff5776, #ff2d55)',
+};
+
+export default node;
