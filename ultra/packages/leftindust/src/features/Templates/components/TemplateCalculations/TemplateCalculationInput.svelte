a033ed6500d8f6e16073bc64d3620c154290864a
Victor <victoroltean09@gmail.com>
2022-07-09 14:36:39 -0700

    Added and registered conditional node.

--- ultra/packages/leftindust/src/features/Templates/components/TemplateCalculations/TemplateCalculationInput.svelte
+++ ultra/packages/leftindust/src/features/Templates/components/TemplateCalculations/TemplateCalculationInput.svelte
@@ -13,6 +13,7 @@
   import TemplateOutputNode from '@/features/Node/components/TemplateOutputNode';
   import MathNode from '@/features/Node/components/MathNode';
   import type { MenuNodes } from '@/features/Nodes';
+  import ConditionalNode from '@/features/Node/components/ConditionalNode';
 
   export let index: number;
   export let calculations: TemplateCalculationWithInstance[];
@@ -37,6 +38,7 @@
     input: TemplateInputNode,
     output: TemplateOutputNode,
     Math: MathNode,
+    Condition: ConditionalNode,
   };
 
   const menuNodes: MenuNodes = [{
@@ -50,6 +52,10 @@
       title: 'Math',
       description: 'Addition, subtraction, multiplication, and division',
       blueprint: MathNode,
+    }, {
+      title: 'Condition',
+      description: 'Equals, not equals, greater than, less than',
+      blueprint: ConditionalNode,
     }],
   }];
 
