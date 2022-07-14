544eb7a1e537a945ba30718725c3737e8a8df298
Victor <victoroltean09@gmail.com>
2022-07-13 22:30:42 -0700

    Update SurveyTemplate annotations.

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/SurveyTemplateEntityToSurveyTemplateConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/SurveyTemplateEntityToSurveyTemplateConverter.kt
@@ -5,14 +5,14 @@ import com.leftindust.mockingbird.NullEntityIdInConverterException
 import org.springframework.stereotype.Component
 import java.util.UUID
 
-@Component
-class SurveyTemplateEntityToSurveyTemplateConverter: InfallibleConverter<SurveyTemplateEntity, SurveyTemplate> {
-    override fun convert(source: SurveyTemplateEntity): SurveyTemplate {
-        return SurveyTemplateImpl(
-            id = source.id ?: throw NullEntityIdInConverterException(source)
-        )
-    }
-
-    private class SurveyTemplateImpl(override val id: UUID) : SurveyTemplate {
-    }
-}
+//@Component
+//class SurveyTemplateEntityToSurveyTemplateConverter: InfallibleConverter<SurveyTemplateEntity, SurveyTemplate> {
+//    override fun convert(source: SurveyTemplateEntity): SurveyTemplate {
+//        return SurveyTemplateImpl(
+//            id = source.id ?: throw NullEntityIdInConverterException(source)
+//        )
+//    }
+//
+//    private class SurveyTemplateImpl(override val id: UUID) : SurveyTemplate {
+//    }
+//}
