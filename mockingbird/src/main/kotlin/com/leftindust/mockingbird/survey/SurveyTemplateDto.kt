32ef12895a5bcbcd9295f01f757fb4a075d40cc5
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-07-23 12:58:45 -0700

    added a todo and a test to make pass for SurveyTemplateQueryController

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/SurveyTemplateDto.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/SurveyTemplateDto.kt
@@ -1,12 +1,13 @@
 package com.leftindust.mockingbird.survey
 
 import com.leftindust.mockingbird.graphql.AbstractGraphQLDto
+import com.leftindust.mockingbird.survey.SurveyTemplateDto.SurveyTemplateDtoId
 import java.util.UUID
 
 data class SurveyTemplateDto(
-    override val id: Id,
+    override val id: SurveyTemplateDtoId,
     val title: String,
-    val subtitle: String? = null,
-) : AbstractGraphQLDto<SurveyTemplateDto.Id>() {
-    data class Id(override val value: UUID) : GraphQLID<UUID>
+    val subtitle: String?,
+) : AbstractGraphQLDto<SurveyTemplateDtoId>() {
+    data class SurveyTemplateDtoId(override val value: UUID) : GraphQLID<UUID>
 }
