caf8eb5ed5a47d3c5708daa378f092d99a50e30b
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-08-05 00:51:06 -0700

    added getting completedSurveys from SurveyLink

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/ReadCompleteSurveyService.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/ReadCompleteSurveyService.kt
@@ -1,7 +1,9 @@
 package com.leftindust.mockingbird.survey.complete
 
 import com.leftindust.mockingbird.survey.complete.CompleteSurveyDto.CompleteSurveyDtoId
+import com.leftindust.mockingbird.survey.link.SurveyLinkDto
 
 interface ReadCompleteSurveyService {
     suspend fun completeSurveyByCompleteSurveyId(completeSurveyId: CompleteSurveyDtoId): CompleteSurvey?
+    suspend fun getBySurveyLink(surveyLinkDtoId: SurveyLinkDto.SurveyLinkDtoId): List<CompleteSurvey>?
 }
--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/ReadCompleteSurveyServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/ReadCompleteSurveyServiceImpl.kt
@@ -1,6 +1,8 @@
 package com.leftindust.mockingbird.survey.complete
 
 import com.leftindust.mockingbird.InfallibleConverter
+import com.leftindust.mockingbird.survey.link.SurveyLinkDto
+import com.leftindust.mockingbird.survey.link.SurveyLinkRepository
 import javax.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Service
 @Service
 class ReadCompleteSurveyServiceImpl(
     private val completeSurveyRepository: CompleteSurveyRepository,
+    private val surveyLinkRepository: SurveyLinkRepository,
     private val completeSurveyEntityToCompleteSurvey: InfallibleConverter<CompleteSurveyEntity, CompleteSurvey>
 ) : ReadCompleteSurveyService {
     override suspend fun completeSurveyByCompleteSurveyId(completeSurveyId: CompleteSurveyDto.CompleteSurveyDtoId): CompleteSurvey? {
         val completeSurveyEntity = completeSurveyRepository.findByIdOrNull(completeSurveyId.value)
             ?: return null
         return completeSurveyEntityToCompleteSurvey.convert(completeSurveyEntity)
+    }
+
+    override suspend fun getBySurveyLink(surveyLinkDtoId: SurveyLinkDto.SurveyLinkDtoId): List<CompleteSurvey>? {
+        val completeSurveyEntities = completeSurveyRepository.findBySurveyLink_Id(surveyLinkDtoId.value)
+        return completeSurveyEntities.map { completeSurveyEntityToCompleteSurvey.convert(it) }
     }
 }
