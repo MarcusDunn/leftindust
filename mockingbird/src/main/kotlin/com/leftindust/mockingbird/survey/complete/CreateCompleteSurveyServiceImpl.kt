c66cf574854d0aae44e07e03dea379f886416332
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-08-05 00:18:28 -0700

    added getting surveyTemplates from filled out surveys

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CreateCompleteSurveyServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CreateCompleteSurveyServiceImpl.kt
@@ -1,15 +1,20 @@
 package com.leftindust.mockingbird.survey.complete
 
+import com.leftindust.mockingbird.survey.template.SurveyTemplateRepository
 import javax.transaction.Transactional
+import mu.KotlinLogging
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Transactional
 @Service
 class CreateCompleteSurveyServiceImpl(
     private val completeSurveyRepository: CompleteSurveyRepository,
+    private val surveyTemplateRepository: SurveyTemplateRepository,
     private val completeSurveyEntityToCompleteSurvey: CompleteSurveyEntityToCompleteSurvey,
 ) : CreateCompleteSurveyService {
-    override suspend fun createCompleteSurvey(createCompleteSurvey: CreateCompleteSurvey): CompleteSurvey {
+    private val logger = KotlinLogging.logger {  }
+    override suspend fun createCompleteSurvey(createCompleteSurvey: CreateCompleteSurvey): CompleteSurvey? {
         val newCompleteSurvey = CompleteSurveyEntity(
             sections = createCompleteSurvey.completeSurveyTemplateSections
                 .map { createCompleteSurveySection ->
@@ -23,7 +28,9 @@ class CreateCompleteSurveyServiceImpl(
                             .toSet()
                     )
                 }
-                .toSet()
+                .toSet(),
+            surveyTemplate = surveyTemplateRepository.findByIdOrNull(createCompleteSurvey.surveyTemplateId.value)
+                ?: return null.also { logger.debug { "Did not find a surveyTemplate with id [${createCompleteSurvey.surveyTemplateId.value}] while creating $createCompleteSurvey" } }
         )
         val completeSurveyEntity = completeSurveyRepository.save(newCompleteSurvey)
         return completeSurveyEntityToCompleteSurvey.convert(completeSurveyEntity)
--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/template/ReadSurveyTemplateServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/template/ReadSurveyTemplateServiceImpl.kt
@@ -5,6 +5,8 @@ import com.leftindust.mockingbird.InfallibleConverter
 import com.leftindust.mockingbird.graphql.types.input.Range
 import com.leftindust.mockingbird.graphql.types.input.toPageable
 import com.leftindust.mockingbird.patient.ReadPatientService
+import com.leftindust.mockingbird.survey.complete.CompleteSurveyDto
+import com.leftindust.mockingbird.survey.complete.CompleteSurveyRepository
 import javax.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service
 @Transactional
 class ReadSurveyTemplateServiceImpl(
     val surveyTemplateRepository: SurveyTemplateRepository,
+    val completeSurveyRepository: CompleteSurveyRepository,
     val readPatientService: ReadPatientService,
     val surveyTemplateEntityToSurveyTemplateConverter: InfallibleConverter<SurveyTemplateEntity, SurveyTemplate>
 ) : ReadSurveyTemplateService {
@@ -25,5 +28,11 @@ class ReadSurveyTemplateServiceImpl(
     override suspend fun getByRange(range: Range): List<SurveyTemplate> {
         val surveyTemplateEntities = surveyTemplateRepository.findAll(range.toPageable())
         return surveyTemplateEntities.map { surveyTemplateEntityToSurveyTemplateConverter.convert(it) }.toList()
+    }
+
+    override suspend fun getByCompletedSurvey(completeSurveyId: CompleteSurveyDto.CompleteSurveyDtoId): SurveyTemplate? {
+        val completeSurveyEntity = completeSurveyRepository.findByIdOrNull(completeSurveyId.value)
+            ?: return null
+        return surveyTemplateEntityToSurveyTemplateConverter.convert(completeSurveyEntity.surveyTemplate)
     }
 }
