b3da41858dd9c20a57c9155d2cd3f95bcf783556
Aydan Gaite <aksg823@gmail.com>
2022-08-05 23:19:01 -0700

    Setup wizards for patient/doctor create

---
+++ ultra/packages/leftindust/src/features/Doctor/DoctorWizard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Wizard from '../Wizard/Wizard.svelte';
+</script>
+
+<Wizard
+  title="New Doctor"
+  subtitle="Add a new doctor to your clinic"
+  color="purple"
+/>
---
+++ ultra/packages/leftindust/src/features/Patient/PatientWizard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Wizard from '../Wizard/Wizard.svelte';
+</script>
+
+<Wizard
+  title="New Patient"
+  subtitle="Add a new patient to your clinic"
+  color="purple"
+/>
