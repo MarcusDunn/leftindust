1b14eba8110dbbd73cc3ee1dbc4cac0db6e157ea
briantkho <briantkho@gmail.com>
2022-08-09 23:24:39 -0700

    Added create patient/doctor wizard form

--- ultra/packages/leftindust/src/features/Doctor/DoctorWizard.svelte
+++ ultra/packages/leftindust/src/features/Doctor/DoctorWizard.svelte
@@ -1,9 +1,130 @@
 <script lang="ts">
+  import type { Data } from '@/api/server/graphql' 
+  import type { Popover } from 'framework7/types'
+
+  import type { NameInfo, DoctorInput } from '@/api/server/graphql/schema/leftindust.schema'
+  // import type {DoctorEngine as DoctorEngineType}
+
+  import { Country } from '@/api/server/graphql/schema/leftindust.schema'
+  import { WidgetType } from '../Widgets';
+
+  import { writable } from 'svelte/store';
+  import { _ } from '@/language';
+  import { loginForm } from '../Account';
+  import { closeWizard } from '../Wizard';
+  // import DoctorEngine from '';
+
+  // import { DoctorMutationDocument } from '@/api/server';
+  import getNativeAPI from '@/api/bridge';
+  import deepmerge from 'deepmerge';
+
+  import { Row, Col, Block } from 'framework7-svelte';
+
+  import Input from '../Input/Input.svelte';
   import Wizard from '../Wizard/Wizard.svelte';
+  import Add from '../Input/components/Add/Add.svelte'
+  // import Phone from '../Phone/Phone.svelte';
+
+  import { AttachmentsTabs } from '../Attachments'; //!!!
+  import SpecificGrid from '../Widgets/components/Grid/SpecificGrid.svelte';
+  import { clientsSelectedTab } from '@/features/Clients/store'
+  import { ClientsTab } from '@/features/Clients/index'
+
+  export let data: Data<'Doctor'> | undefined = undefined;
+
+  const { Dialog } = getNativeAPI();
+
+  let doctor: any = writable();
+  let disabled = true;
+
+  let input: Partial<DoctorInput> & { nameInfo: NonNullable<NameInfo>} = {
+    nameInfo:{
+      firstName: '',
+      middleName: '',
+      lastName: '',
+    },
+    title: '',
+    addresses: [],
+    patients: [],
+    emails: [],
+    phones: [],
+  }
+  
+  let patients = writable<Data[]>([]);
+
+  let patientsPopover: Popover.Popover;
+
+  const edit = (): void => {
+    input = deepmerge(input, {
+      ...$doctor,
+      nameInfo: {
+        firstName: $doctor.firstName,
+        middleName: $doctor.middleName,
+        lastName: $doctor.lastName,
+      },
+    });
+  }
+
+  const submit = () => {}
+
+  $: if ($doctor) edit();
+
+  const { form, errors } = loginForm();
+
 </script>
 
-<Wizard
-  title="New Doctor"
-  subtitle="Add a new doctor to your clinic"
-  color="purple"
-/>
+<!-- <AttachmentsTabs 
+  attachments={patients}
+  types={['Patient']}
+  bind:instance={patientsPopover}
+/> -->
+
+<form use:form>
+  <Wizard
+    title={$_('generics.newDoctor')}
+    subtitle={$_('descriptions.addDoctorDescription')}
+    color="purple"
+    {disabled}
+    on:submit={submit}
+  >
+    <Block style="margin-top: 60px">
+      <Block>
+        <h4>Identification</h4>
+        <Row>
+          <Col xlarge="50" width="100">
+            <Row>
+              <Col width="100" medium="33">
+                <Input>
+                  <input type="text" placeholder="First Name" bind:value={input.nameInfo.firstName} />
+                </Input>
+                <p />
+              </Col>
+              <Col width="100" medium="33">
+                <Input>
+                  <input type="text" placeholder="Middle Name (Optional)" bind:value={input.nameInfo.middleName} />
+                </Input>
+                <p />
+              </Col>
+              <Col width="100" medium="33">
+                <Input>
+                  <input type="text" placeholder="Last Name" bind:value={input.nameInfo.lastName} />
+                </Input>
+                <p />
+              </Col>
+            </Row>
+          </Col>
+          <Col xlarge="50" width="100">
+            <Row>
+              <Col width="100">
+                <Input>
+                  <input type="text" placeholder="Title" bind:value={input.title} />
+                </Input>
+                <p />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Block>
+    </Block>
+  </Wizard>
+</form>
--- ultra/packages/leftindust/src/features/Patient/PatientWizard.svelte
+++ ultra/packages/leftindust/src/features/Patient/PatientWizard.svelte
@@ -1,9 +1,171 @@
 <script lang="ts">
+  import type { Data } from '@/api/server/graphql';
+  import type { Popover } from 'framework7/types';
+
+  import type {
+    PatientInput,
+    NameInfo,
+    PatientEditInput,
+  } from '@/api/server/graphql/schema/leftindust.schema';
+
+  import {
+    Sex,
+    Ethnicity,
+    Month,
+    Relationship,
+    Country,
+  } from '@/api/server/graphql/schema/leftindust.schema';
+  import { WidgetType } from '../Widgets';
+
+  import { writable } from 'svelte/store';
+  import { _ } from '@/language';
+  import { loginForm } from '../Account';
+  import { closeWizard } from '../Wizard';
+
+  import getNativeAPI from '@/api/bridge';
+  import deepmerge from 'deepmerge';
+
+  import { Row, Col, Block } from 'framework7-svelte';
+
+  import Input from '../Input/Input.svelte';
+  import Select from '../Input/components/Select/Select.svelte'
   import Wizard from '../Wizard/Wizard.svelte';
+  import Add from '../Input/components/Add/Add.svelte';
+
+  import SpecificGrid from '../Widgets/components/Grid/SpecificGrid.svelte';
+  import { clientsSelectedTab } from '@/features/Clients/store';
+  import { ClientsTab } from '@/features/Clients/index';
+
+  export let data: Data<'Patient'> | undefined = undefined;
+
+  const { Dialog } = getNativeAPI();
+
+  let patient: any = writable();
+  let disabled = true;
+
+  let input: Partial<PatientInput | PatientEditInput> & { nameInfo: NonNullable<NameInfo>} = {
+    nameInfo: {
+      firstName: '',
+      middleName: '',
+      lastName: '',
+    },
+    ethnicity: undefined,
+    dateOfBirth: undefined,
+    sex: undefined,
+    gender: undefined,
+    insuranceNumber: '',
+    addresses: [],
+    doctors: [],
+    emails: [],
+    emergencyContacts: [],
+    phones: [],
+  };
+
+  let doctors = writable<Data[]>([]);
+  
+  let doctorsPopover: Popover.Popover;
+
+  const edit = () => {
+    input = deepmerge(input, {
+      ...$patient,
+      nameInfo: {
+        firstName: $patient.firstName,
+        middleName: $patient.middleName,
+        lastName: $patient.lastName,
+      },
+    });
+  };
+
+  const submit = () => {};
+
+  const { form, errors } = loginForm();
 </script>
 
-<Wizard
-  title="New Patient"
-  subtitle="Add a new patient to your clinic"
-  color="purple"
-/>
+<form use:form>
+  <Wizard
+    title={$_("generics.newPatient")}
+    subtitle={$_("descriptions.addPatientDescription")}
+    color="purple"
+    {disabled}
+    on:submit={submit}
+  >
+    {#key $patient}
+      <Block style="margin-top: 60px">
+        <Block>
+          <h4>Identification</h4>
+          <Row>
+            <Col xlarge="50" width="100">
+              <Row>              
+                <Col width="100" medium="33">
+                <Input>
+                  <input type="text" placeholder="First Name" bind:value={input.nameInfo.firstName} />
+                </Input>
+                <p />
+              </Col>
+              <Col width="100" medium="33">
+                <Input>
+                  <input type="text" placeholder="Middle Name (Optional)" bind:value={input.nameInfo.middleName} />
+                </Input>
+                <p />
+              </Col>
+              <Col width="100" medium="33">
+                <Input>
+                  <input type="text" placeholder="Last Name" bind:value={input.nameInfo.lastName} />
+                </Input>
+                <p />
+              </Col>
+              </Row>
+              <Row>
+                <Col width="100" medium="50">
+                  <Select
+                  title="Ethnicity"
+                    options={[
+                      {
+                        text: 'American Aboriginal',
+                        value: Ethnicity.AmericanAboriginal,
+                      },
+                      {
+                        text: 'Asian',
+                        value: Ethnicity.Asian,
+                      },
+                      {
+                        text: 'Black',
+                        value: Ethnicity.Black,
+                      },
+                      {
+                        text: 'Hispanic',
+                        value: Ethnicity.Hispanic,
+                      },
+                      {
+                        text: 'Pacific Islander',
+                        value: Ethnicity.PacificIslander,
+                      },
+                      {
+                        text: 'White',
+                        value: Ethnicity.White,
+                      },
+                    ]}
+                    bind:value={input.ethnicity}
+                  />
+                  <p />
+                </Col>
+                <Col width="100" medium="50">
+                  <Input>
+                    <input type="text" placeholder="Gender Identity (Optional)" bind:value={input.gender} />
+                  </Input>
+                  <p />
+                </Col>
+              </Row>
+              <Col width="100">
+                <Input>
+                  <input type="text" placeholder="Insurance Number (Optional)" bind:value={input.insuranceNumber} />
+                </Input>
+                <p />
+              </Col>
+            </Col>
+          </Row>
+        </Block>
+      </Block>
+    {/key}
+  </Wizard>
+</form>
