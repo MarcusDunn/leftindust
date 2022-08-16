9ba51ed3bda001b43c7ff14de6c2de26b9533a80
boris <borisv.ca@gmail.com>
2022-08-15 22:07:21 -0700

    added and fixed tests for patient controllers

---
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/patient/PatientEmailQueryControllerTest.kt
@@ -0,0 +1,62 @@
+package com.leftindust.mockingbird.patient
+
+import com.leftindust.mockingbird.email.EmailDto
+import com.leftindust.mockingbird.email.ReadEmailService
+import com.leftindust.mockingbird.util.EmailMother
+import com.leftindust.mockingbird.util.PatientMother
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.coEvery
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.graphql.test.tester.GraphQlTester
+import org.springframework.security.web.server.SecurityWebFilterChain
+import java.util.*
+
+@GraphQlTest(controllers = [PatientQueryController::class, PatientEmailQueryController::class])
+internal class PatientEmailQueryControllerTest(
+    @Autowired private val graphQlTester: GraphQlTester
+) {
+    @MockkBean
+    private lateinit var httpSecurity: SecurityWebFilterChain
+
+    @MockkBean
+    private lateinit var readPatientService: ReadPatientService
+
+    @MockkBean
+    private lateinit var readEmailService: ReadEmailService
+
+    @Test
+    internal fun `check can query for phone fields`() {
+        coEvery { readPatientService.getByPatientId(PatientMother.Dan.graphqlId) } returns PatientMother.Dan.entityPersisted
+        coEvery { readEmailService.getPatientEmails(PatientMother.Dan.graphqlId) } returns listOf(EmailMother.DansEmail.entityPersisted)
+
+        @Language("graphql")
+        val query = """
+            query {
+                patientsByPatientId(patientIds: [{ value: "${PatientMother.Dan.id}" }]) {
+                    emails {
+                        id { value }
+                        email
+                        type
+                    }
+                }
+            }
+        """.trimIndent()
+
+
+        graphQlTester.document(query)
+            .execute()
+            .errors()
+            .verify()
+            .path("patientsByPatientId[0].emails[*].id.value")
+            .entity(object : ParameterizedTypeReference<List<UUID>>() {})
+            .matches { it.contains(EmailMother.DansEmail.dto.id.value) }
+            .path("patientsByPatientId[0].emails[0]")
+            .entity(EmailDto::class.java)
+            .isEqualTo(EmailMother.DansEmail.dto)
+
+    }
+}
