414953aae70f2379d7b527472f77e11b0636812b
boris <borisv.ca@gmail.com>
2022-08-15 22:05:46 -0700

    removed converters

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/patient/PatientToPatientDtoConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/patient/PatientToPatientDtoConverter.kt
@@ -14,21 +14,13 @@ import com.leftindust.mockingbird.phone.Phone_
 import org.springframework.stereotype.Component
 
 @Component
-class PatientToPatientDtoConverter(
-    private val phoneToPhoneDtoConverter: InfallibleConverter<Phone, PhoneDto>,
-    private val emailToEmailDtoConverter: InfallibleConverter<Email, EmailDto>,
-    private val addressToAddressDtoConverter: InfallibleConverter<Address, AddressDto>
-) : InfallibleConverter<Patient, PatientDto> {
+class PatientToPatientDtoConverter : InfallibleConverter<Patient, PatientDto> {
     override fun convert(source: Patient): PatientDto {
         return PatientDto(
             id = PatientDto.PatientDtoId(source.id!!),
             firstName = source.nameInfo.firstName,
             middleName = source.nameInfo.middleName,
             lastName = source.nameInfo.lastName,
-            addresses = source.addresses.map { addressToAddressDtoConverter.convert(it) }.toList(),
-            emails = source.emails.map { emailToEmailDtoConverter.convert(it) }.toList(),
-            phoneNumbers = source.phones.map { phoneToPhoneDtoConverter.convert(it) }.toList(),
-            thumbnail = source.thumbnail,
             dateOfBirth = source.dateOfBirth,
             insuranceNumber = source.insuranceNumber,
             sex = source.sex,
