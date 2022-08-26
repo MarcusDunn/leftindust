af2902a6a475fdae6631c88f1f5000494e2c13f7
ashgozli <ak23b24@gmail.com>
2022-08-25 20:44:05 -0700

    Moving methods from `Clinic(Domain)` to `ClinicEntity` and adjusted `ClinicDoctorEntity` clinic value to Entity.

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/clinic/ClinicEntity.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/clinic/ClinicEntity.kt
@@ -2,14 +2,20 @@
 
 package com.leftindust.mockingbird.clinic
 
+import com.leftindust.mockingbird.AddedElementMessage
+import com.leftindust.mockingbird.RemovedElementMessage
 import com.leftindust.mockingbird.address.Address
 import com.leftindust.mockingbird.doctor.ClinicDoctorEntity
+import com.leftindust.mockingbird.doctor.Doctor
 import com.leftindust.mockingbird.persistance.AbstractJpaPersistable
+import mu.KotlinLogging
 import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.OneToMany
 import javax.persistence.OneToOne
+
+private val logger = KotlinLogging.logger { }
 
 @Entity
 class ClinicEntity(
@@ -19,4 +25,22 @@ class ClinicEntity(
     var address: Address,
     @OneToMany(mappedBy = "clinic")
     var doctors: MutableSet<ClinicDoctorEntity> = mutableSetOf(),
-) : AbstractJpaPersistable()
+) : AbstractJpaPersistable() {
+    fun clearDoctors() {
+        doctors.forEach { removeDoctor(it.doctor) }
+    }
+
+    fun addDoctor(doctor: Doctor) {
+        val clinicDoctor = ClinicDoctorEntity(this, doctor)
+        doctors.add(clinicDoctor)
+        doctor.clinics.add(clinicDoctor)
+        logger.trace { AddedElementMessage(doctor, doctor::clinics, clinicDoctor) }
+    }
+
+    fun removeDoctor(doctor: Doctor) {
+        val clinicDoctor = ClinicDoctorEntity(this, doctor)
+        doctors.remove(clinicDoctor)
+        doctor.clinics.remove(clinicDoctor)
+        logger.trace { RemovedElementMessage(doctor, doctor::clinics, clinicDoctor) }
+    }
+}
