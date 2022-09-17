e3194412a33dfd0420e2117832c18b6adbba2201
o0victor0o <victoroltean09@gmail.com>
2022-09-16 23:16:07 -0700

    Fixed ClinicMutationControllerWebTest

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/clinic/ClinicRepository.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/clinic/ClinicRepository.kt
@@ -3,6 +3,5 @@ package com.leftindust.mockingbird.clinic
 import java.util.UUID
 import org.springframework.data.repository.CrudRepository
 
-
 interface ClinicRepository : CrudRepository<ClinicEntity, UUID> {
 }
