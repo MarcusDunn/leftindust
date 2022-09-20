32c08ff7a0f649e4fd50213e1e74ad5142d3b6f9
o0victor0o <victoroltean09@gmail.com>
2022-09-19 22:06:38 -0700

    Introduced Doctor domain to separate doctor entity from domain objects.

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/doctor/Doctor.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/doctor/Doctor.kt
@@ -1,5 +1,9 @@
 package com.leftindust.mockingbird.doctor
 
+import com.leftindust.mockingbird.person.NameInfo
+import java.time.LocalDate
+import java.util.UUID
+
 interface Doctor {
     val id: UUID
     val nameInfo: NameInfo
