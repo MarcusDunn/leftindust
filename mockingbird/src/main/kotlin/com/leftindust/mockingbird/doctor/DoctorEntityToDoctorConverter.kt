ea31f886ed04e368863ef57b47f423a1c8753b6d
o0victor0o <victoroltean09@gmail.com>
2022-09-19 22:06:02 -0700

    Introduced Doctor domain to separate doctor entity from domain objects.

---
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/doctor/DoctorEntityToDoctorConverter.kt
@@ -0,0 +1,29 @@
+package com.leftindust.mockingbird.doctor
+
+import com.leftindust.mockingbird.InfallibleConverter
+import com.leftindust.mockingbird.NullEntityIdInConverterException
+import com.leftindust.mockingbird.person.NameInfo
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.util.UUID
+
+@Component
+class DoctorEntityToDoctorConverter : InfallibleConverter<DoctorEntity, Doctor> {
+    override fun convert(source: DoctorEntity): Doctor {
+        return DoctorImpl(
+            id = source.id ?: throw NullEntityIdInConverterException(source),
+            nameInfo = source.nameInfo,
+            thumbnail = source.thumbnail,
+            title = source.title,
+            dateOfBirth = source.dateOfBirth,
+        )
+    }
+
+    private data class DoctorImpl(
+        override val id: UUID,
+        override val nameInfo: NameInfo,
+        override val thumbnail: ByteArray?,
+        override val title: String?,
+        override val dateOfBirth: LocalDate?
+    ) : Doctor
+}
