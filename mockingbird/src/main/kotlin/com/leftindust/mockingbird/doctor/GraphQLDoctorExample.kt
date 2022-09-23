e053dc4791c36fff8c5568cb4f215242d9da145b
o0victor0o <victoroltean09@gmail.com>
2022-09-22 21:15:24 -0700

    Updated GraphQLDoctorExample types.

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/doctor/GraphQLDoctorExample.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/doctor/GraphQLDoctorExample.kt
@@ -3,6 +3,7 @@ package com.leftindust.mockingbird.doctor
 import com.leftindust.mockingbird.graphql.types.search.Example
 import com.leftindust.mockingbird.graphql.types.search.combineWithStrict
 import com.leftindust.mockingbird.graphql.types.search.filter.CaseAgnosticStringFilterDto
+import com.leftindust.mockingbird.person.NameInfo_
 import javax.persistence.criteria.CriteriaBuilder
 import javax.persistence.criteria.Predicate
 import javax.persistence.criteria.Root
@@ -11,14 +12,13 @@ data class GraphQLDoctorExample(
     val firstName: CaseAgnosticStringFilterDto? = null,
     val lastName: CaseAgnosticStringFilterDto? = null,
     override val strict: Boolean,
-) : Example<Doctor> {
-    override fun toPredicate(criteriaBuilder: CriteriaBuilder, root: Root<Doctor>): Predicate {
-//        val doctorNameInfo = root.join(Doctor_.nameInfo)
-//        val predicates = listOfNotNull(
-//            firstName?.toPredicate(criteriaBuilder, doctorNameInfo, NameInfo_.firstName),
-//            lastName?.toPredicate(criteriaBuilder, doctorNameInfo, NameInfo_.lastName),
-//        ).toTypedArray()
-//        return combineWithStrict(criteriaBuilder, *predicates)
-        TODO("Update to work with DoctorEntity")
+) : Example<DoctorEntity> {
+    override fun toPredicate(criteriaBuilder: CriteriaBuilder, root: Root<DoctorEntity>): Predicate {
+        val doctorNameInfo = root.join(DoctorEntity_.nameInfo)
+        val predicates = listOfNotNull(
+            firstName?.toPredicate(criteriaBuilder, doctorNameInfo, NameInfo_.firstName),
+            lastName?.toPredicate(criteriaBuilder, doctorNameInfo, NameInfo_.lastName),
+        ).toTypedArray()
+        return combineWithStrict(criteriaBuilder, *predicates)
     }
 }
