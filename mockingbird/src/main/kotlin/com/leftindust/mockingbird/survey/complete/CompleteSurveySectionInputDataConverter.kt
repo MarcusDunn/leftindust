3a19d0f87775fdca76f62f297c864aecc185c3b4
boris <borisv.ca@gmail.com>
2022-09-23 12:42:48 -0700

    fixed tests

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CompleteSurveySectionInputDataConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CompleteSurveySectionInputDataConverter.kt
@@ -1,15 +1,15 @@
 package com.leftindust.mockingbird.survey.complete
 
-import com.fasterxml.jackson.annotation.JsonSubTypes
-import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
-@Converter(autoApply = true)
+@Converter
 class CompleteSurveySectionInputDataConverter(
     @Autowired
+    @Qualifier("jsonMapper")
     val jacksonObjectMapper: ObjectMapper
 ) : AttributeConverter<CompleteSurveySectionInputData, String> {
     override fun convertToDatabaseColumn(attribute: CompleteSurveySectionInputData): String {
