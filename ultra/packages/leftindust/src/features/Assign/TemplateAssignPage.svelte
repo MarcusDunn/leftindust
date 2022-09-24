8438028187b544f1e912dd1b497490d60cc1faae
Aydan Gaite <aksg823@gmail.com>
2022-09-23 18:18:53 -0700

    continuing to prototype assign

--- ultra/packages/leftindust/src/features/Assign/TemplateAssignPage.svelte
+++ ultra/packages/leftindust/src/features/Assign/TemplateAssignPage.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { defaultRangeInput, PartialPatientsByRangeQueryDocument, type PartialPatientFragment } from '@/api/server';
   import { operationStore, query } from '@urql/svelte';
-  import { Button, Col, Icon, Row } from 'framework7-svelte';
+  import { Block, Button, Col, Icon, Row } from 'framework7-svelte';
   import { writable } from 'svelte/store';
   import DescriptivePlaceholder from '../App/components/DescriptivePlaceholder/DescriptivePlaceholder.svelte';
   import PatientCheckboxCell from '../Patient/components/PatientCell/PatientCheckboxCell.svelte';
@@ -15,6 +15,8 @@
   const selected = writable<string[]>([]);
   let patients: PartialPatientFragment[];
 
+  let generated = false;
+
   const request = operationStore(PartialPatientsByRangeQueryDocument, {
     range: defaultRangeInput,
   });
@@ -27,57 +29,66 @@
 <Page>
   <svelte:fragment slot="fixed">
     <Appbar close={{ popup: true }} />
-    <Footer>
+    {#if !generated}
+      <Footer>
+        <div
+          style="
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 25px;
+            margin-bottom: 25px;
+            padding-left: 10px;
+            padding-right: 10px;
+          "
+        >
+          <Button
+            disabled={patients.length === 0 || $selected.length === 0}
+            large
+            fill
+            color="deeppurple"
+            style="width: 260px;"
+            on:click={() => (generated = true)}
+          >
+            <Icon style="margin-right: 4px" f7="link" /> Generate Link
+          </Button>
+        </div>
+      </Footer>
+    {/if}
+  </svelte:fragment>
+  {#if !generated}
+    {#if patients.length > 0}
+      <Row class="no-gutter">
+        {#each patients as patient}
+          <Col width="33" style="margin-bottom: 10px">
+            <Cells>
+              <PatientCheckboxCell {patient} bind:selected={$selected} />
+            </Cells>
+          </Col>
+        {/each}
+      </Row>
+    {:else}
       <div
         style="
           display: flex;
           flex-direction: column;
           align-items: center;
-          margin-top: 25px;
-          margin-bottom: 25px;
-          padding-left: 10px;
-          padding-right: 10px;
+          justify-content: center;
+          height: calc(100% - 120px);
         "
       >
-        <Button
-          disabled={patients.length === 0 || $selected.length === 0}
-          large
-          fill
-          color="deeppurple"
-          style="width: 260px;"
-        >
-          <Icon style="margin-right: 4px" f7="link" /> Generate Link
-        </Button>
+        <DescriptivePlaceholder 
+          title={$_('generics.noPatients')} 
+          description="To generate a link for a patient, you must first create a patient"
+          link = {{
+            label: $_('descriptions.learnMorePatients'),
+          }}
+        />
       </div>
-    </Footer>
-  </svelte:fragment>
-  {#if patients.length > 0}
-    <Row class="no-gutter">
-      {#each patients as patient}
-        <Col width="33" style="margin-bottom: 10px">
-          <Cells>
-            <PatientCheckboxCell {patient} bind:selected={$selected} />
-          </Cells>
-        </Col>
-      {/each}
-    </Row>
+    {/if}
   {:else}
-    <div
-      style="
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: calc(100% - 120px);
-      "
-    >
-      <DescriptivePlaceholder 
-        title={$_('generics.noPatients')} 
-        description="To generate a link for a patient, you must first create a patient"
-        link = {{
-          label: $_('descriptions.learnMorePatients'),
-        }}
-      />
-    </div>
+    <Block>
+
+    </Block>
   {/if}
 </Page>
