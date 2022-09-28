5d1ca112a8981451c62be2aa30f775f99cccf8a7
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-09-27 17:10:08 -0700

    added email value class and tests

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/extensions/ConcurrentFlows.kt
+++
@@ -1,9 +0,0 @@
-package com.leftindust.mockingbird.extensions
-
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-
-suspend fun <A, B> Iterable<A>.parallelMap(f: suspend (A) -> B): List<B> = coroutineScope {
-    map { async { f(it) } }.awaitAll()
-}
---
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/validate/Email.kt
@@ -0,0 +1,19 @@
+package com.leftindust.mockingbird.validate
+
+import dev.forkhandles.values.StringValueFactory
+import dev.forkhandles.values.Value
+import dev.forkhandles.values.regex
+
+/**
+ * Taken from http://emailregex.com/
+ */
+private const val emailRegex =
+    "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+
+@JvmInline
+value class Email private constructor(override val value: String) : Value<String> {
+    companion object : StringValueFactory<Email>(
+        fn = ::Email,
+        validation = emailRegex.regex,
+    )
+}
