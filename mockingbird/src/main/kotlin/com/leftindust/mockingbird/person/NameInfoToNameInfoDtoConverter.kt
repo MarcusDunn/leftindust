718acf05b3b080cad2bb2859eeb97d18012d8ba4
kelvin <kelvincao6@gmail.com>
2022-10-03 19:57:16 -0700

    Add id to NameInfo

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/person/NameInfoToNameInfoDtoConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/person/NameInfoToNameInfoDtoConverter.kt
@@ -5,10 +5,10 @@ import com.leftindust.mockingbird.NullEntityIdInConverterException
 import org.springframework.stereotype.Component
 
 @Component
-class NameInfoToNameInfoDtoConverter : InfallibleConverter<NameInfoEntity, NameInfoDto> {
-    override fun convert(source: NameInfoEntity): NameInfoDto {
+class NameInfoToNameInfoDtoConverter : InfallibleConverter<NameInfo, NameInfoDto> {
+    override fun convert(source: NameInfo): NameInfoDto {
         return NameInfoDto(
-            NameInfoDto.NameInfoDtoId(source.id ?: throw NullEntityIdInConverterException(source)),
+            NameInfoDto.NameInfoDtoId(source.id ?: throw Exception("Expected non-null NameInfo id")),
             source.firstName,
             source.middleName,
             source.lastName
