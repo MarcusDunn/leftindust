9fec504c967137573fec167017905fc1414ed689
kelvin <kelvincao6@gmail.com>
2022-10-03 19:57:16 -0700

    Add id to NameInfo

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/person/NameInfo.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/person/NameInfo.kt
@@ -1,6 +1,9 @@
 package com.leftindust.mockingbird.person
 
+import java.util.UUID
+
 interface NameInfo {
+    val id: UUID?
     val firstName: String
     val lastName: String
     val middleName: String?
