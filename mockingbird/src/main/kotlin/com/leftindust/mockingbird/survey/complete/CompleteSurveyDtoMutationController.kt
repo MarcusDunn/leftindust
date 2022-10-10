740ec7fa3288737cbf90fceef73fa8af1ae6e328
boris <borisv.ca@gmail.com>
2022-10-09 21:48:47 -0700

    added result return type to create complete survey service

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CompleteSurveyDtoMutationController.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CompleteSurveyDtoMutationController.kt
@@ -3,9 +3,13 @@ package com.leftindust.mockingbird.survey.complete
 import com.leftindust.mockingbird.FallibleConverter
 import com.leftindust.mockingbird.InconvertibleDtoException
 import com.leftindust.mockingbird.InfallibleConverter
+import dev.forkhandles.result4k.get
+import dev.forkhandles.result4k.onFailure
+import dev.forkhandles.result4k.resultFrom
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
+import javax.persistence.PersistenceException
 
 @Controller
 class CompleteSurveyDtoMutationController(
@@ -14,10 +18,9 @@ class CompleteSurveyDtoMutationController(
     private val completeSurveyToCompleteSurveyDtoConverter: InfallibleConverter<CompleteSurvey, CompleteSurveyDto>,
 ) {
     @MutationMapping("createCompleteSurvey")
-    suspend fun createCompleteSurvey(@Argument("createCompleteSurvey") createCompleteSurveyDto: CreateCompleteSurveyDto): CompleteSurveyDto? {
+    suspend fun createCompleteSurvey(@Argument("createCompleteSurvey") createCompleteSurveyDto: CreateCompleteSurveyDto): CompleteSurveyDto {
         val createCompleteSurvey = createCompleteSurveyDtoToCreateCompleteSurveyConverter.convert(createCompleteSurveyDto)
-        val completeSurvey = createCompleteSurveyService.createCompleteSurvey(createCompleteSurvey ?: throw InconvertibleDtoException<CreateCompleteSurvey>(createCompleteSurveyDto))
-            ?: return null
+        val completeSurvey = createCompleteSurveyService.createCompleteSurvey(createCompleteSurvey ?: throw InconvertibleDtoException<CreateCompleteSurvey>(createCompleteSurveyDto)).onFailure { throw it.get() }
         return completeSurveyToCompleteSurveyDtoConverter.convert(completeSurvey)
     }
 }
--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CreateCompleteSurveyServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/complete/CreateCompleteSurveyServiceImpl.kt
@@ -1,11 +1,21 @@
 package com.leftindust.mockingbird.survey.complete
 
+import com.leftindust.mockingbird.EntityNotFoundException
+import com.leftindust.mockingbird.MockingbirdException
+import com.leftindust.mockingbird.survey.link.SurveyLink
 import com.leftindust.mockingbird.survey.link.SurveyLinkRepository
 import com.leftindust.mockingbird.survey.template.SurveyTemplateRepository
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.resultFrom
+import dev.forkhandles.values.Value
 import javax.transaction.Transactional
 import mu.KotlinLogging
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import javax.persistence.PersistenceException
 
 @Transactional
 @Service
@@ -16,7 +26,7 @@ class CreateCompleteSurveyServiceImpl(
     private val completeSurveyEntityToCompleteSurvey: CompleteSurveyEntityToCompleteSurvey,
 ) : CreateCompleteSurveyService {
     private val logger = KotlinLogging.logger { }
-    override suspend fun createCompleteSurvey(createCompleteSurvey: CreateCompleteSurvey): CompleteSurvey? {
+    override suspend fun createCompleteSurvey(createCompleteSurvey: CreateCompleteSurvey): Result4k<CompleteSurvey, MockingbirdException> {
         val newCompleteSurvey = CompleteSurveyEntity(
             sections = createCompleteSurvey.completeSurveyTemplateSections
                 .map { createCompleteSurveySection ->
@@ -24,7 +34,7 @@ class CreateCompleteSurveyServiceImpl(
                         inputs = createCompleteSurveySection.completedSurveyInputs
                             .map { createCompleteSurveyInput ->
                                 CompleteSurveySectionInputEntity(
-                                    value =  createCompleteSurveyInput.value
+                                    value = createCompleteSurveyInput.value
                                 )
                             }
                             .toSet()
@@ -33,10 +43,16 @@ class CreateCompleteSurveyServiceImpl(
                 .toSet(),
             surveyLink = run {
                 surveyLinkRepository.findByIdOrNull(createCompleteSurvey.surveyLinkId.value)
-                    ?: return null.also { logger.debug { "Did not find a surveyTemplateLink with id [${createCompleteSurvey.surveyLinkId.value}] while creating $createCompleteSurvey" } }
+                    ?: return Failure(
+                        EntityNotFoundException(
+                            createCompleteSurvey.surveyLinkId.value,
+                            SurveyLink::class
+                        )
+                    )
+                        .also { logger.debug { "Did not find a surveyTemplateLink with id [${createCompleteSurvey.surveyLinkId.value}] while creating $createCompleteSurvey" } }
             }
         )
         val completeSurveyEntity = completeSurveyRepository.save(newCompleteSurvey)
-        return completeSurveyEntityToCompleteSurvey.convert(completeSurveyEntity)
+        return Success(completeSurveyEntityToCompleteSurvey.convert(completeSurveyEntity))
     }
 }
