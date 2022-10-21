231aa06730637c729bfe37752f8beccba7f9083d
kelvin <kelvincao6@gmail.com>
2022-10-21 11:21:17 -0700

    Fix NullSubQueryException

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/patient/PatientNameInfoQueryController.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/patient/PatientNameInfoQueryController.kt
@@ -1,7 +1,6 @@
 package com.leftindust.mockingbird.patient
 
 import com.leftindust.mockingbird.NullSubQueryException
-import com.leftindust.mockingbird.email.ReadEmailService
 import com.leftindust.mockingbird.person.ReadNameInfoService
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
@@ -13,19 +12,19 @@ class PatientNameInfoQueryController(
     @SchemaMapping(typeName = PatientDto.GRAPHQL_TYPE, field = "firstName")
     suspend fun firstName(patientDto: PatientDto): String {
         return readNameInfoService.getByPatientId(patientDto.id)?.firstName
-            ?: throw NullSubQueryException(patientDto, ReadEmailService::getPatientEmails)
+            ?: throw NullSubQueryException(patientDto, ReadNameInfoService::getByPatientId)
     }
 
     @SchemaMapping(typeName = PatientDto.GRAPHQL_TYPE, field = "middleName")
     suspend fun middleName(patientDto: PatientDto): String {
         return readNameInfoService.getByPatientId(patientDto.id)?.middleName
-            ?: throw NullSubQueryException(patientDto, ReadEmailService::getPatientEmails)
+            ?: throw NullSubQueryException(patientDto, ReadNameInfoService::getByPatientId)
     }
 
     @SchemaMapping(typeName = PatientDto.GRAPHQL_TYPE, field = "lastName")
     suspend fun lastName(patientDto: PatientDto): String {
         return readNameInfoService.getByPatientId(patientDto.id)?.lastName
-            ?: throw NullSubQueryException(patientDto, ReadEmailService::getPatientEmails)
+            ?: throw NullSubQueryException(patientDto, ReadNameInfoService::getByPatientId)
     }
 
 
