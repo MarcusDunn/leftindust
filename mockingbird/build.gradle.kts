2f73432b1bd0ccb0b3ca66ad393a7fff41c8d9f5
Kelvin Cao <kelvincao6@gmail.com>
2022-11-14 11:06:19 -0800

    Removed unused dependency

--- mockingbird/build.gradle.kts
+++ mockingbird/build.gradle.kts
@@ -27,9 +27,8 @@ repositories {
 }
 
 dependencies {
+    // spring cloud
     implementation("io.awspring.cloud:spring-cloud-starter-aws-ses:2.4.2")
-    implementation("io.awspring.cloud:spring-cloud-aws-autoconfigure:2.4.2")
-
     // spring
     implementation("org.springframework.boot", "spring-boot-starter-mail")
     implementation("org.springframework.boot", "spring-boot-starter-webflux")
