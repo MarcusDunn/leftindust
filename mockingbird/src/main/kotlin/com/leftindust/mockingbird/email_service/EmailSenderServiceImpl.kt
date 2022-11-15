655b41fae96dfcbc6c4e971485b0772feca56695
Kelvin Cao <kelvincao6@gmail.com>
2022-11-14 17:20:21 -0800

    Use coroutines

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/email_service/EmailSenderServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/email_service/EmailSenderServiceImpl.kt
@@ -1,9 +1,9 @@
 package com.leftindust.mockingbird.email_service
 
 import com.leftindust.mockingbird.validate.EmailAddress
+import kotlinx.coroutines.*
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import javax.mail.internet.MimeMessage
 
@@ -11,18 +11,22 @@ import javax.mail.internet.MimeMessage
 class EmailSenderServiceImpl(
     private val emailSender: JavaMailSender
 ): EmailSenderService {
-    @Async
     override suspend fun sendHtmlEmail(subject: String, html: String, targetEmails: List<EmailAddress>) {
-        emailSender.send(*targetEmails.map {
-            val message: MimeMessage = emailSender.createMimeMessage()
-            val helper = MimeMessageHelper(message, "utf-8")
+        withContext(Dispatchers.IO) {
+            targetEmails.map {
+                async {
+                    val message: MimeMessage = emailSender.createMimeMessage()
+                    val helper = MimeMessageHelper(message, "utf-8")
 
-            helper.setFrom("hello@leftindust.com")
-            helper.setSubject(subject)
-            helper.setText(html, true)
+                    helper.setFrom("hello@leftindust.com")
+                    helper.setSubject(subject)
+                    helper.setText(html, true)
 
-            helper.setTo(it.value)
-            return@map message
-        }.toTypedArray())
+                    helper.setTo(it.value)
+                    emailSender.send(message)
+                }
+            }.awaitAll()
+        }
     }
+
 }
