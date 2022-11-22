e7c32b4e0dac97ccf2a1c9413ef3f0a48f25f533
Kelvin Cao <kelvincao6@gmail.com>
2022-11-22 13:07:53 -0800

    Added documentation

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/email_service/EmailSenderService.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/email_service/EmailSenderService.kt
@@ -3,11 +3,29 @@ package com.leftindust.mockingbird.email_service
 import com.leftindust.mockingbird.validate.EmailAddress
 
 interface EmailSenderService {
+    /**
+     * Send an HTML email using AWS Simple Email Service.
+     *
+     * @param html Email body in HTML.
+     * @param targetEmails Addresses to send to. Addresses must be verified when
+     * running in the SES sandbox.
+     * @param from Address to send from. Address must be verified when running
+     * in the SES sandbox.
+     */
     suspend fun sendHtmlEmail(
         subject: String,
         html: String,
         targetEmails: List<EmailAddress>,
         from: String = "hello@leftindust.com")
+    /**
+     * Send an HTML email using AWS Simple Email Service.
+     *
+     * @param html Email body in HTML.
+     * @param targetEmails Address to send to. Address must be verified when
+     * running in the SES sandbox.
+     * @param from Address to send from. Address must be verified when running
+     * in the SES sandbox.
+     */
     suspend fun sendHtmlEmail(
         subject: String,
         html: String,
