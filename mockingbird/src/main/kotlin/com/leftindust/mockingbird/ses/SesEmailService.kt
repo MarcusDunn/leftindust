74fa701304e888917c392c0d37b17e4dba72b3b5
Kelvin Cao <kelvincao6@gmail.com>
2022-11-23 18:39:06 -0800

    Moved extension function

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/ses/SesEmailService.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/ses/SesEmailService.kt
@@ -18,4 +18,24 @@ interface SesEmailService {
         targetEmails: List<EmailAddress>,
         from: EmailAddress
     )
+
+    companion object {
+        /**
+         * Send an HTML email using AWS Simple Email Service.
+         *
+         * @param html HTML email body.
+         * @param targetEmail Address to send to. Address must be verified when
+         * running in the SES sandbox.
+         * @param from Address to send from. Address must be verified when running
+         * in the SES sandbox.
+         */
+        suspend fun SesEmailService.sendHtmlEmail(
+            subject: String,
+            html: String,
+            targetEmail: EmailAddress,
+            from: EmailAddress
+        ) {
+            sendHtmlEmail(subject, html, listOf(targetEmail), from)
+        }
+    }
 }
