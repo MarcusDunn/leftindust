b9812f5e51e5c7e61511f3bfa8ac58ae9a1a192d
boris <borisv.ca@gmail.com>
2022-11-24 23:36:23 -0800

    added test annotations

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/thymeleaf/ThymeleafServiceTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/thymeleaf/ThymeleafServiceTest.kt
@@ -1,17 +1,23 @@
 package com.leftindust.mockingbird.thymeleaf
 
+import com.leftindust.mockingbird.MockingbirdApplication
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.approvaltests.Approvals
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.thymeleaf.context.Context
 import org.thymeleaf.spring5.SpringTemplateEngine
 import java.util.*
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension::class)
+@Import(value = [SpringTemplateEngine::class])
 internal class ThymeleafServiceTest {
 
     @Autowired
@@ -20,7 +26,6 @@ internal class ThymeleafServiceTest {
     @org.junit.jupiter.api.Test
     fun thymeleafTemplateTest() = runTest {
         val context = Context(Locale.ENGLISH, mapOf("hello" to "bow hello"))
-        print(context)
         val html = templateEngine.process("TemplateSample", context).toString()
         Approvals.verify(html)
     }
