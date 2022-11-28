4453c2d6db5404f257664d7940e2f288a1315938
o0victor0o <victoroltean09@gmail.com>
2022-11-27 19:40:02 -0800

    Unused import

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/sns/SnsTextServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/sns/SnsTextServiceImpl.kt
@@ -4,7 +4,7 @@ import com.amazonaws.services.sns.AmazonSNS
 import com.amazonaws.services.sns.model.PublishRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import javax.transaction.Transactional
+
 @Service
 class SnsTextServiceImpl(@Autowired private val snsClient: AmazonSNS) : SnsTextService {
     override suspend fun publishSMS(phoneNumber: String, message: String) {
