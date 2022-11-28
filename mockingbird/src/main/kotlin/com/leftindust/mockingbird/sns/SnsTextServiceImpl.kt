511b58e077fd12607661e76b38943f1be2a149d7
o0victor0o <victoroltean09@gmail.com>
2022-11-27 19:38:16 -0800

    Removed transactional annotation.

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/sns/SnsTextServiceImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/sns/SnsTextServiceImpl.kt
@@ -5,9 +5,7 @@ import com.amazonaws.services.sns.model.PublishRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
-
 @Service
-@Transactional
 class SnsTextServiceImpl(@Autowired private val snsClient: AmazonSNS) : SnsTextService {
     override suspend fun publishSMS(phoneNumber: String, message: String) {
         snsClient.publish(PublishRequest()
