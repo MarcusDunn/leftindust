2fbbfe4c0f398af28a370cf02d1f7c555b97a690
o0victor0o <victoroltean09@gmail.com>
2022-11-27 19:12:26 -0800

    Test SNS text service.

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/sns/SnsTextMessageTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/sns/SnsTextMessageTest.kt
@@ -1,26 +1,28 @@
 package com.leftindust.mockingbird.sns
 
-import com.ninjasquad.springmockk.MockkBean
+import com.amazonaws.services.sns.AmazonSNS
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.security.web.server.SecurityWebFilterChain
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
-internal class SnsTextMessageTest(@Autowired private val snsTextService: SnsTextService) {
-    @MockkBean
-    private lateinit var httpSecurity: SecurityWebFilterChain
-
+internal class SnsTextMessageTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    internal fun `SNS text works properly`() = runTest {
-        val textMessage = "Test from leftindust"
-        val phoneNumber = ""
+    internal fun `Test SNS sends an SMS message`() = runTest {
+        val snsClient = mockk<AmazonSNS>(relaxed = true) {
+            coEvery { publish(any()) } returns mockk()
+        }
+        val snsTextService = SnsTextServiceImpl(snsClient)
 
-        if(phoneNumber.isNotEmpty()) snsTextService.publishSMS(phoneNumber, textMessage)
+        snsTextService.publishSMS("16042225555", "test message")
+
+        verify(exactly = 1) { snsClient.publish(any()) }
     }
 }
