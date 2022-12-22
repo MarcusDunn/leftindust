f14ef6d69797ebd3099507889cb70d01414488ac
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-21 22:16:38 -0800

    Refactored version of ClinicToClinicDtoConverter.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/clinic/ClinicQueryController.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/clinic/ClinicQueryController.kt
@@ -1,6 +1,5 @@
 package com.leftindust.mockingbird.clinic
 
-import com.leftindust.mockingbird.InfallibleConverter
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
@@ -8,12 +7,11 @@ import org.springframework.stereotype.Controller
 @Controller
 class ClinicQueryController(
     private val readClinicService: ReadClinicService,
-    private val clinicToClinicDtoConverter: InfallibleConverter<Clinic, ClinicDto>,
 ) {
 
     @QueryMapping
     suspend fun clinicByClinicId(@Argument clinicId: ClinicDto.ClinicDtoId): ClinicDto? {
         val clinic = readClinicService.getByClinicId(clinicId)
-        return clinic?.let { clinicToClinicDtoConverter.convert(it) }
+        return clinic?.let { it.toClinicDto() }
     }
 }
