0f83686dc46f50597738077afe6216abea5ca997
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-24 19:24:29 -0800

    Refactored version of EventToEventDtoConverter.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/event/EventQueryController.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/event/EventQueryController.kt
@@ -11,6 +11,7 @@ import com.leftindust.mockingbird.patient.ReadPatientService
 import com.leftindust.mockingbird.visit.ReadVisitService
 import com.leftindust.mockingbird.visit.Visit
 import com.leftindust.mockingbird.visit.VisitDto
+import dev.forkhandles.result4k.onFailure
 import mu.KotlinLogging
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -19,7 +20,6 @@ import org.springframework.stereotype.Controller
 @Controller
 class EventQueryController(
     private val readEventService: ReadEventService,
-    private val eventToEventDtoConverter: InfallibleConverter<Event, EventDto>,
 ) {
     private val logger = KotlinLogging.logger { }
 
@@ -27,55 +27,57 @@ class EventQueryController(
     suspend fun eventsByIds(events: List<EventDto.EventDtoId>): List<EventDto?> {
         return events
             .map { readEventService.getByEventId(it) }
-            .map { it?.let { event -> eventToEventDtoConverter.convert(event) } }
+            .map { it?.let { event -> event.toEventDto().onFailure { throw it.reason.toMockingbirdException() } } }
     }
 
     @QueryMapping
     suspend fun eventsByDoctorId(@Argument doctorId: DoctorDto.DoctorDtoId): List<EventDto>? {
         val events = readEventService.getByDoctorId(doctorId) ?: return null
-        return events.map { eventToEventDtoConverter.convert(it) }
+        return events.map { it.toEventDto().onFailure { throw it.reason.toMockingbirdException() } }
     }
 
     @QueryMapping
     suspend fun eventsByPatientId(@Argument patientId: PatientDto.PatientDtoId): List<EventDto>? {
         val events = readEventService.getByPatientId(patientId) ?: return null
-        return events.map { eventToEventDtoConverter.convert(it) }
+        return events.map {
+            it.toEventDto().onFailure { throw it.reason.toMockingbirdException() }
+        }
     }
-}
 
-@Controller
-class EventPatientController(
-    private val readPatientService: ReadPatientService,
-    private val patientToPatientDtoConverter: InfallibleConverter<Patient, PatientDto>,
-) {
-    @QueryMapping
-    suspend fun patients(eventDto: EventDto): List<PatientDto> {
-        val patients = readPatientService.getByEvent(eventDto.id)
-            ?: throw NullSubQueryException(eventDto, ReadPatientService::getByEvent)
-        return patients.map { patientToPatientDtoConverter.convert(it) }
+    @Controller
+    class EventPatientController(
+        private val readPatientService: ReadPatientService,
+        private val patientToPatientDtoConverter: InfallibleConverter<Patient, PatientDto>,
+    ) {
+        @QueryMapping
+        suspend fun patients(eventDto: EventDto): List<PatientDto> {
+            val patients = readPatientService.getByEvent(eventDto.id)
+                ?: throw NullSubQueryException(eventDto, ReadPatientService::getByEvent)
+            return patients.map { patientToPatientDtoConverter.convert(it) }
+        }
     }
-}
 
-@Controller
-class EventDoctorController(
-    private val readDoctorService: ReadDoctorService,
-    private val doctorToDoctorDtoConverter: InfallibleConverter<Doctor, DoctorDto>,
-) {
-    @QueryMapping
-    suspend fun doctors(eventDto: EventDto): List<DoctorDto> {
-        TODO()
+    @Controller
+    class EventDoctorController(
+        private val readDoctorService: ReadDoctorService,
+        private val doctorToDoctorDtoConverter: InfallibleConverter<Doctor, DoctorDto>,
+    ) {
+        @QueryMapping
+        suspend fun doctors(eventDto: EventDto): List<DoctorDto> {
+            TODO()
+        }
     }
-}
 
-@Controller
-class EventVisitController(
-    private val readVisitService: ReadVisitService,
-    private val visitToVisitDtoConverter: InfallibleConverter<Visit, VisitDto>,
-) {
-    @QueryMapping
-    suspend fun visit(eventDto: EventDto): VisitDto? {
-        return readVisitService
-            .findByEvent(eventDto.id)
-            ?.let { visitToVisitDtoConverter.convert(it) }
+    @Controller
+    class EventVisitController(
+        private val readVisitService: ReadVisitService,
+        private val visitToVisitDtoConverter: InfallibleConverter<Visit, VisitDto>,
+    ) {
+        @QueryMapping
+        suspend fun visit(eventDto: EventDto): VisitDto? {
+            return readVisitService
+                .findByEvent(eventDto.id)
+                ?.let { visitToVisitDtoConverter.convert(it) }
+        }
     }
 }
