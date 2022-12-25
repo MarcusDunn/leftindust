b689577ca7a87ec1274125fe51620c8448d8a03a
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-24 20:22:48 -0800

    Refactored version of CreateContactDtoToCreateContactConverter.kt & ContactDtoConverter.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/contact/ContactDtoConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/contact/ContactDtoConverter.kt
@@ -1,18 +1,19 @@
 package com.leftindust.mockingbird.contact
 
-import com.leftindust.mockingbird.InfallibleConverter
-import org.springframework.stereotype.Component
+import com.leftindust.mockingbird.ConversionError
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
 
-@Component
-class ContactDtoConverter : InfallibleConverter<Contact, ContactDto> {
-    override fun convert(source: Contact): ContactDto {
-        val contactId = requireNotNull(source.id) { "Contact id cannot be null" }
-        return ContactDto(
+
+fun Contact.toContactDto(): Result4k<ContactDto, ConversionError<Contact, ContactDto>> {
+    val contactId = requireNotNull(this.id) { "Contact id cannot be null" }
+    return Success(
+        ContactDto(
             id = ContactDto.ContactDtoId(contactId),
-            firstName = source.nameInfoEntity.firstName,
-            middleName = source.nameInfoEntity.middleName,
-            lastName = source.nameInfoEntity.lastName,
-            relationship = source.relationship,
+            firstName = this.nameInfoEntity.firstName,
+            middleName = this.nameInfoEntity.middleName,
+            lastName = this.nameInfoEntity.lastName,
+            relationship = this.relationship,
         )
-    }
+    )
 }
