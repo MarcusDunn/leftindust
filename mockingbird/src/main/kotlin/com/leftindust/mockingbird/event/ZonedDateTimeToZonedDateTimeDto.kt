c803b735dbc195485c105e789cd5538fdd543a5e
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-24 19:55:09 -0800

    Refactored version of ZonedDateTimeToZonedDateTimeDto.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/event/ZonedDateTimeToZonedDateTimeDto.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/event/ZonedDateTimeToZonedDateTimeDto.kt
@@ -1,12 +1,12 @@
 package com.leftindust.mockingbird.event
 
-import com.leftindust.mockingbird.InfallibleConverter
+import com.leftindust.mockingbird.ConversionError
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
 import java.time.ZonedDateTime
-import org.springframework.stereotype.Component
 
-@Component
-class ZonedDateTimeToZonedDateTimeDto : InfallibleConverter<ZonedDateTime, ZonedDateTimeDto> {
-    override fun convert(source: ZonedDateTime): ZonedDateTimeDto {
-        return ZonedDateTimeDto(source.toString())
-    }
+fun ZonedDateTime.toZonedDateTimeDto(): Result4k<ZonedDateTimeDto, ConversionError<ZonedDateTime, ZonedDateTimeDto>> {
+    return Success(
+        ZonedDateTimeDto(this.toString())
+    )
 }
