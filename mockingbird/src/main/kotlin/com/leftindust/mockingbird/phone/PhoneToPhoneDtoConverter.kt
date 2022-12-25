1bbfa4fce792dc784919aa7ff64709c41a8bbb7f
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-24 20:09:53 -0800

    Refactored version of PhoneToPhoneDtoConverter.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/phone/PhoneToPhoneDtoConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/phone/PhoneToPhoneDtoConverter.kt
@@ -1,17 +1,20 @@
 package com.leftindust.mockingbird.phone
 
-import com.leftindust.mockingbird.InfallibleConverter
-import org.springframework.stereotype.Component
+import com.leftindust.mockingbird.ConversionError
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
 
-@Component
-class PhoneToPhoneDtoConverter : InfallibleConverter<Phone, PhoneDto> {
-    override fun convert(source: Phone): PhoneDto {
-        return PhoneDto(
-            PhoneDto.PhoneDtoId(source.id ?: throw IllegalArgumentException("Source ${Phone::class.simpleName} must have an id but was $source")),
-            source.number,
-            source.type
+fun Phone.toPhoneDto(): Result4k<PhoneDto, ConversionError<Phone, PhoneDto>> {
+    return Success(
+        PhoneDto(
+            PhoneDto.PhoneDtoId(
+                this.id
+                    ?: throw IllegalArgumentException("Source ${Phone::class.simpleName} must have an id but was $this")
+            ),
+            this.number,
+            this.type
         )
-    }
+    )
 }
 
 
