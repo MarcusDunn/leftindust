932025260c37a8c9b8d0831ade58c0851335482b
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2022-12-26 17:27:40 -0800

    Refactored: SurveyTemplateCalculationEntityToSurveyTemplateCalculationConverter.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/template/SurveyTemplateCalculationEntityToSurveyTemplateCalculationConverter.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/survey/template/SurveyTemplateCalculationEntityToSurveyTemplateCalculationConverter.kt
@@ -1,29 +1,30 @@
 package com.leftindust.mockingbird.survey.template
 
-import com.leftindust.mockingbird.InfallibleConverter
+import com.leftindust.mockingbird.ConversionError
 import com.leftindust.mockingbird.NullEntityIdInConverterException
-import java.util.UUID
-import org.springframework.stereotype.Component
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import java.util.*
 
-@Component
-class SurveyTemplateCalculationEntityToSurveyTemplateCalculationConverter : InfallibleConverter<SurveyTemplateCalculationEntity, SurveyTemplateCalculation> {
-    override fun convert(source: SurveyTemplateCalculationEntity): SurveyTemplateCalculation {
-        return SurveyTemplateCalculationImpl(
-            id = source.id ?: throw NullEntityIdInConverterException(source),
-            index = source.index,
-            label = source.label,
-            inputType = source.inputType,
-            showOnComplete = source.showOnComplete,
-            calculation = source.calculation,
+
+fun SurveyTemplateCalculationEntity.toSurveyTemplateCalculation(): Result4k<SurveyTemplateCalculation, ConversionError<SurveyTemplateCalculationDto, SurveyTemplateCalculation>> {
+    return Success(
+        SurveyTemplateCalculationImpl(
+            id = id ?: throw NullEntityIdInConverterException(this),
+            index = index,
+            label = label,
+            inputType = inputType,
+            showOnComplete = showOnComplete,
+            calculation = calculation,
         )
-    }
-
-    private data class SurveyTemplateCalculationImpl(
-        override val id: UUID,
-        override val index: Int,
-        override val label: String,
-        override val inputType: SurveyTemplateInputType,
-        override val showOnComplete: Boolean,
-        override val calculation: String,
-    ) : SurveyTemplateCalculation
+    )
 }
+
+private data class SurveyTemplateCalculationImpl(
+    override val id: UUID,
+    override val index: Int,
+    override val label: String,
+    override val inputType: SurveyTemplateInputType,
+    override val showOnComplete: Boolean,
+    override val calculation: String,
+) : SurveyTemplateCalculation
