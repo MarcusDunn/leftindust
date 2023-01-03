846ef7bb380e4bc4729f4b2b6f12c79cd0d9f004
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2023-01-03 13:51:55 -0800

    Refactored UserAccountDetailsToUserAccountDetailsDtoConverter.kt

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/user/MediqUserUserAccountDetailsController.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/user/MediqUserUserAccountDetailsController.kt
@@ -1,19 +1,18 @@
 package com.leftindust.mockingbird.user
 
-import com.leftindust.mockingbird.InfallibleConverter
 import com.leftindust.mockingbird.NullSubQueryException
+import dev.forkhandles.result4k.onFailure
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
 class MediqUserUserAccountDetailsController(
     private val readUserAccountDetailsService: ReadUserAccountDetailsService,
-    private val userAccountDetailsToUserAccountDetailsDtoConverter: InfallibleConverter<UserAccountDetails, UserAccountDetailsDto>,
 ) {
     @QueryMapping
     fun accountDetails(mediqUser: MediqUserDto): UserAccountDetailsDto {
         val userAccountDetails = readUserAccountDetailsService.getUserInfoByMediqUserUniqueId(mediqUser.id)
             ?: throw NullSubQueryException(mediqUser, ReadUserAccountDetailsService::getUserInfoByMediqUserUniqueId)
-        return userAccountDetailsToUserAccountDetailsDtoConverter.convert(userAccountDetails)
+        return userAccountDetails.toUserAccountDetailsDto().onFailure { throw it.reason.toMockingbirdException() }
     }
 }
